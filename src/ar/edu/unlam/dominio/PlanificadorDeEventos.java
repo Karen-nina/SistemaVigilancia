e0a4ecb64b228162824c9501f8b3c19de5a2f4f8
Karen <karen_nina4.0@hotmail.com>
2023-06-30 09:07:43 -0300

    first commit

---
+++ src/ar/edu/unlam/dominio/PlanificadorDeEventos.java
@@ -0,0 +1,45 @@
+package ar.edu.unlam.dominio;
+
+public class PlanificadorDeEventos {
+
+	public String getUsuario(String mailOrganizador) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getEvento(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void add(Usuario organizador) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void crear(String usuarioBuscado, Usuario agasajado) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Object getCantidadDeCumpleanios() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getCantidadDeCasamientos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getCantidadDeEventos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getCantidadDeUsuarios() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/ar/edu/unlam/dominio/Usuario.java
@@ -0,0 +1,11 @@
+package ar.edu.unlam.dominio;
+
+public class Usuario {
+
+	
+	
+	public Usuario(String mail, String nombre, Integer edad) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
